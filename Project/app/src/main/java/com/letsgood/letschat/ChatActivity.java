f5d4b081798d1ed66e2bb55228c1bec6f2a344ce
Jakub Nižaradze <nizarjak@fit.cvut.cz>
2015-06-11 12:30:12 +0200

    Styles

---
+++ Project/app/src/main/java/com/letsgood/letschat/ChatActivity.java
@@ -0,0 +1,18 @@
+package com.letsgood.letschat;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+}
